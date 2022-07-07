0e37dc74f8fdd3b98b383cd6f0c023b1d07d72cc
Wachira11ke <100116527+Wachira11ke@users.noreply.github.com>
2022-07-07 09:16:02 +0300

    Submodule Modification

--- downloadpdf.php
+++ downloadpdf.php
@@ -12,3 +12,16 @@ $imagpdf = file_put_contents($image, file_get_contents($file));
 
 echo $imagepdf;
 ?>
+<?php
+
+$file = $_GET["file"] .".pdf";
+
+// We will be outputting a PDF
+header('Content-Type: application/pdf');
+
+// It will be called Delft.pdf
+header('Content-Disposition: attachment; filename="Delft.pdf"');
+
+$imagpdf = file_put_contents($image, file_get_contents($file));
+
+echo $imagepdf;
